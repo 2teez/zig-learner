fba77056f271443145cb94f9f88a9b765fc17e04
timothy adigun <adigun.timothy@gmail.com>
2026-01-01 07:47:21 +0000

    enum practice

--- practiced/errorh.zig
+++ practiced/errorh.zig
@@ -1,5 +1,22 @@
 const std = @import("std");
 
+const Days = enum {
+    Sunday,
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+};
+
 pub fn main() void {
-    std.debug.print("Start from here!\n", .{});
+    const day = Days.Wednesday;
+    if (day == Days.Wednesday) {
+        std.debug.print("Guess it is hums day!\n", .{});
+    }
+
+    // the number of day it is
+    const days_num = @intFromEnum(day) + 1;
+    std.debug.print("It's day number {d} of the week!\n", .{days_num});
 }
