f94facbd5dc48d1d4800c9320a6a0200508e1da3
timothy adigun <adigun.timothy@gmail.com>
2026-01-03 21:36:35 +0000

    using try in error handling in zig

--- junkz/err_handler.zig
+++ junkz/err_handler.zig
@@ -1,5 +1,17 @@
 const std = @import("std");
 
-pub fn main() void {
-    std.debug.print("Start from here!\n", .{});
+const MathErr = error{
+    ZeroDivision,
+};
+
+pub fn main() !void {
+    const result = try division(34, 0);
+    std.debug.print("{}\n", .{result});
+}
+
+fn division(num: i32, deno: i32) !i32 {
+    if (deno == 0) {
+        return MathErr.ZeroDivision;
+    }
+    return @divTrunc(num, deno);
 }
