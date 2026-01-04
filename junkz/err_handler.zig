49e40a26b9c87e19277832ec5d3c4d72eef974bc
timothy adigun <adigun.timothy@gmail.com>
2026-01-04 13:39:00 +0000

    using try and catch in zig lang

--- junkz/err_handler.zig
+++ junkz/err_handler.zig
@@ -4,12 +4,16 @@ const MathErr = error{
     ZeroDivision,
 };
 
-pub fn main() !void {
-    const result = try division(34, 0);
-    std.debug.print("{}\n", .{result});
+//pub fn main() !void {
+pub fn main() void {
+    //const result = try division(34, 0);
+    _ = division(34, 0) catch |err| {
+        std.debug.print("{}\n", .{err});
+        return;
+    };
 }
 
-fn division(num: i32, deno: i32) !i32 {
+fn division(num: i32, deno: i32) MathErr!i32 {
     if (deno == 0) {
         return MathErr.ZeroDivision;
     }
