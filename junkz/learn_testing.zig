a029d9ed69657dc094c2f85644ae2eaa958729f7
timothy adigun <adigun.timothy@gmail.com>
2026-01-07 20:34:11 +0000

    checking error in zig

--- junkz/learn_testing.zig
+++ junkz/learn_testing.zig
@@ -8,8 +8,22 @@ fn square(number: i32) i32 {
     return number * number;
 }
 
+fn parseInt(value: []const u8) !i32 {
+    return std.fmt.parseInt(i32, value, 10);
+}
+
 test "square function should return the square of the number" {
-    std.testing.expect(square(3) == 9);
-    std.testing.expect(square(-4) == 16);
-    std.testing.expect(square(0) == 0);
+    try std.testing.expect(square(3) == 9);
+    try std.testing.expect(square(-4) == 16);
+    try std.testing.expect(square(0) == 0);
+}
+
+test "parse a string to a number. The test should fail" {
+    const invalid_type = "not a number";
+    const result = parseInt(invalid_type);
+    try std.testing.expectError(std.fmt.ParseIntError.InvalidCharacter, result);
+}
+
+test "this test is not ready" {
+    return error.SkipZigTest;
 }
