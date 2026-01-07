c44e728e9fc4d1c16f4f779e7ebf1c4e855735ca
timothy adigun <adigun.timothy@gmail.com>
2026-01-07 19:49:03 +0000

    write a test function in zig

--- junkz/learn_testing.zig
+++ junkz/learn_testing.zig
@@ -3,3 +3,13 @@ const std = @import("std");
 pub fn main() void {
     std.debug.print("Start from here!\n", .{});
 }
+
+fn square(number: i32) i32 {
+    return number * number;
+}
+
+test "square function should return the square of the number" {
+    std.testing.expect(square(3) == 9);
+    std.testing.expect(square(-4) == 16);
+    std.testing.expect(square(0) == 0);
+}
