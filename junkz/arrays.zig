b42cdc04f31a4e9cb65c94ea15c4336e7e375bf1
timothy adigun <adigun.timothy@gmail.com>
2026-01-09 23:33:50 +0000

    using arraying and testing it

--- junkz/arrays.zig
+++ junkz/arrays.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const expect = std.testing.expect;
 
 pub fn main() void {
     const msg = [_]u8{ 'h', 'e', 'l', 'l', 'o' };
@@ -9,4 +10,23 @@ pub fn main() void {
         assert(mem.eql(u8, &msg, &alt_msg));
         assert(msg.len == 5);
     }
+}
+
+test "iterate over an array" {
+    const greet = "hello";
+    var sum: usize = 0;
+    for (greet) |byte| {
+        sum += byte;
+    }
+
+    try std.testing.expect(sum == 'h' + 'e' + 'l' * 2 + 'o');
+}
+
+test "modify an array" {
+    var numbers: [100]i32 = undefined;
+    for (&numbers, 0..) |*nums, idx| {
+        nums.* = @intCast(idx);
+    }
+    try expect(numbers[99] == 99);
+    try expect(numbers[10] == 10);
 }
