fa8c6cadb18d20ee528e67936f857cb02411d1a9
timothy adigun <adigun.timothy@gmail.com>
2026-01-12 20:06:37 +0000

    still practicing zig array slice

--- junkz/arrays2.zig
+++ junkz/arrays2.zig
@@ -3,3 +3,12 @@ const std = @import("std");
 pub fn main() void {
     std.debug.print("Start from here!\n", .{});
 }
+
+test "basic array and slicing" {
+    const array = [_]u16{ 56, 89, 23, 90 };
+    const array_total_len = array.len;
+
+    try std.testing.expect(array_total_len == 4);
+    try std.testing.expect(array[0] == 56);
+    try std.testing.expect(array[3] == 90);
+}
