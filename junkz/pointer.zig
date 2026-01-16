5133cf407f3e5a6c9f5267177286c67c43e47395
timothy adigun <adigun.timothy@gmail.com>
2026-01-16 23:52:18 +0000

    learning pointer in zig

--- junkz/pointer.zig
+++ junkz/pointer.zig
@@ -8,4 +8,10 @@ test "checking pointer in zig" {
     const my_age: i32 = 45;
     const my_age_pt = &my_age;
     try expect(@TypeOf(my_age_pt) == *const i32);
+    var other_number: i32 = 150;
+    const other_number_pt = &other_number;
+    try expect(@TypeOf(other_number_pt) == *i32);
+    other_number_pt.* -= 50;
+
+    try expect(other_number == 100);
 }
