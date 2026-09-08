43e446dffbb202ff0482a659751a6ff565dcde51
timothy adigun <adigun.timothy@gmail.com>
2026-09-08 20:22:01 +0100

    passing function as a value

--- learning_zig/ch02/func.zig
+++ learning_zig/ch02/func.zig
@@ -7,8 +7,31 @@ pub fn main() void {
         break :blk add(a, b);
     };
     std.debug.print("Result: {}\n", .{result});
+    const result2 = divide(5, 9);
+    if (result2) |r| {
+        std.debug.print("Result2: {}\n", .{r});
+    } else {
+        std.debug.print("Result2: null\n", .{});
+    }
+
+    const result3 = mathOperations(divide, 10, 0);
+    if (result3) |r| {
+        std.debug.print("Result3: {}\n", .{r});
+    } else {
+        std.debug.print("Result3: null\n", .{});
+    }
 }
 
+const func = fn (i32, i32) ?i32;
+fn mathOperations(op: func, a: i32, b: i32) ?i32 {
+    return op(a, b);
+}
+fn divide(a: i32, b: i32) ?i32 {
+    if (b == 0) {
+        return null;
+    }
+    return @divTrunc(a, b);
+}
 pub fn add(a: i32, b: i32) i32 {
     return a + b;
 }
