2aa615c18ce4cb3023941641b4fe67f63f0f9aea
timothy adigun <adigun.timothy@gmail.com>
2026-09-08 15:18:30 +0100

    using two iterators in zig

--- learning_zig/ch01/forloops.zig
+++ learning_zig/ch01/forloops.zig
@@ -8,4 +8,12 @@ pub fn main() void {
         result += item;
     }
     print("The sum of {any} numbers is {}\n", .{ items, result });
+    // using two iterators
+    const items2 = [_]i32{ 2, 4, 6, 8, 10 };
+    const items3 = [_]i32{ 1, 3, 5, 7, 9 };
+    var total: i32 = 0;
+    for (items2, items3) |item2, item3| {
+        total += item2 + item3;
+    }
+    print("The total is {}\n", .{total});
 }
