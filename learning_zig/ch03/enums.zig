90061133a89690763f1d970ca03567fa432ccbf3
timothy adigun <adigun.timothy@gmail.com>
2026-09-11 11:03:23 +0100

    using tagged enums, else statement and swtich with payload in zig

--- learning_zig/ch03/enums.zig
+++ learning_zig/ch03/enums.zig
@@ -8,7 +8,19 @@ const Data = union {
 
 const Result = union(enum) {
     Success: i32,
-    Error: [] const u8,
+    Error: []const u8,
+};
+
+// tagged union can have methods too like enum and struct
+const TaggedUnionWithFunction = union(enum) {
+    data: i32,
+    message: []const u8,
+    pub fn isData(self: TaggedUnionWithFunction) bool {
+        return switch (self) {
+            .data => true,
+            else => false,
+        };
+    }
 };
 
 pub fn main() void {
@@ -20,15 +32,20 @@ pub fn main() void {
     std.debug.print("value: {s}\n", .{value.stringValue});
     // using a tagged union in zig
     const res = calculateValue(-5);
-    switch(res) {
-        .Success => |result| std.debug.print("Succes value is {}", .{result}),
-        .Error => |err_msg| std.debug.print("Error {s}", .{err_msg}),
+    switch (res) {
+        .Success => |result| std.debug.print("Succes value is {}\n", .{result}),
+        .Error => |err_msg| std.debug.print("Error {s}\n", .{err_msg}),
     }
+    // check tagged enum with function
+    const tuwf = TaggedUnionWithFunction{ .data = 56 };
+    if (tuwf.isData()) {
+        std.debug.print("Data value {}\n", .{tuwf.data});
+    } 
 }
 
 pub fn calculateValue(value: i32) Result {
     if (value >= 0) {
-        return Result {.Success = value * 2 };
+        return Result{ .Success = value * 2 };
     }
-    return Result {.Error = "value is less than zero." };
+    return Result{ .Error = "value is less than zero." };
 }
