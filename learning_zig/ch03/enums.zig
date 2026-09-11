61c6cead2ad599457863cd5f49c07fa004f1c317
timothy adigun <adigun.timothy@gmail.com>
2026-09-11 10:11:03 +0100

    using tagged union in zig

--- learning_zig/ch03/enums.zig
+++ learning_zig/ch03/enums.zig
@@ -6,6 +6,11 @@ const Data = union {
     stringValue: []const u8,
 };
 
+const Result = union(enum) {
+    Success: i32,
+    Error: [] const u8,
+};
+
 pub fn main() void {
     var value = Data{ .intValue = 42 };
     std.debug.print("value: {d}\n", .{value.intValue});
@@ -13,4 +18,17 @@ pub fn main() void {
     std.debug.print("value: {d}\n", .{value.floatValue});
     value = Data{ .stringValue = "hello Zig" };
     std.debug.print("value: {s}\n", .{value.stringValue});
+    // using a tagged union in zig
+    const res = calculateValue(-5);
+    switch(res) {
+        .Success => |result| std.debug.print("Succes value is {}", .{result}),
+        .Error => |err_msg| std.debug.print("Error {s}", .{err_msg}),
+    }
+}
+
+pub fn calculateValue(value: i32) Result {
+    if (value >= 0) {
+        return Result {.Success = value * 2 };
+    }
+    return Result {.Error = "value is less than zero." };
 }
