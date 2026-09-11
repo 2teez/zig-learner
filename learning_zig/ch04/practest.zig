6c09a3169ae0c5d93a81b30ba87f6c3c9a5ff43b
timothy adigun <adigun.timothy@gmail.com>
2026-09-12 00:56:19 +0100

    using other test functions in zig

--- learning_zig/ch04/practest.zig
+++ learning_zig/ch04/practest.zig
@@ -8,8 +8,18 @@ fn square(num: i32) i32 {
     return num * num;
 }
 
+fn safeSquare(num: i32) !i32 {
+    const ov = @mulWithOverflow(num, num);
+    if (ov[0] != 0) return error.Overflow;
+    return ov[0];
+}
+
 test "square function should return the square of a number" {
     try std.testing.expect(square(3) == 9);
     try std.testing.expect(square(-6) == 36);
     try std.testing.expect(square(0) == 0);
+}
+
+test "safeSquare should return an error on overflow" {
+    try std.testing.expectError(error.Overflow, safeSquare(1000000));
 }
